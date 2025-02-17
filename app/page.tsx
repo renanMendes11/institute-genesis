d5d5784ae8d631d92372216b0b31f81907607fb1
C1330638 Renan Matias Mendes <c1330638@interno.bb.com.br>
2025-02-17 09:13:14 -0300

    start banner slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-// "use client";
+"use client";
 
 import Image from "next/image";
 import logo from "@/public/logoVert01.png";
+import banner1 from "@/public/campanha01.png";
+import banner2 from "@/public/campanha02.png";
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu";
 import Link from "next/link";
-// import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 
 
@@ -17,8 +20,7 @@ export default function Home() {
           <NavigationMenuList>
             <NavigationMenuItem>
               <Link href="/" legacyBehavior passHref >
-                <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
->
+                <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
                   Sobre Nós
                 </NavigationMenuLink>
               </Link>
@@ -33,6 +35,13 @@ export default function Home() {
             <NavigationMenuItem>
               <Link href="/" legacyBehavior passHref>
                 <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
+                  Notícias
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/" legacyBehavior passHref>
+                <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
                   Contato
                 </NavigationMenuLink>
               </Link>
@@ -40,6 +49,55 @@ export default function Home() {
           </NavigationMenuList>
         </NavigationMenu>
       </div>
+      <Carousel  
+        className='w-5/6 mb-12 h-1/2'
+        opts={{
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 10000,
+            stopOnInteraction: true,
+            stopOnMouseEnter: true,
+          }),
+        ]}
+      >
+        <CarouselContent>
+          <CarouselItem>
+            <Link href='/'>
+              <div className='relative mx-auto'>
+                <Image
+                  src={banner1}
+                  alt="Banner"
+                  height='0'
+                  width='0'
+                  sizes='100vw'
+                  className='w-full'
+                />
+              </div>
+            </Link>
+          </CarouselItem>
+
+          <CarouselItem>
+            <Link href='/'>
+              <div className='relative mx-auto'>
+                <Image
+                  src={banner2}
+                  alt="Banner"
+                  height='0'
+                  width='0'
+                  sizes='100vw'
+                  className='w-full'
+                />
+              </div>
+            </Link>
+          </CarouselItem>
+          
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+    </Carousel>
+
       
     </>
   );
