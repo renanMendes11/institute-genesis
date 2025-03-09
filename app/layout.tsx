7280bd557922941e3fe533e6b203a72e29014127
Renan <renanmendes049@gmail.com>
2025-03-09 19:51:04 -0300

    add bootstrap carousel

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
+        />
+
+      </head>
       <body
         className={`${poppins.className} antialiased `}
       >
--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,45 @@
 "use client";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from "next/image";
-import logo from "@/public/logoVert01.png";
+import logo from "@/public/logoHorin01.png";
 import banner1 from "@/public/campanha01.png";
 import banner2 from "@/public/campanha02.png";
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu";
 import Link from "next/link";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
-import Autoplay from "embla-carousel-autoplay";
-
-
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function Home() {
   return (
     <>
-      <div className="flex items-center justify-around ">
-        <Image src={logo} alt="logo" width={100} height={100} className="p-2"/>
+      <div className="flex items-center justify-around">
+        <Image src={logo} alt="logo" width={180} height={180} className="p-2"/>
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
               <Link href="/" legacyBehavior passHref >
-                <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
+                <NavigationMenuLink className="text-md text-gray-700 no-underline group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
                   Sobre Nós
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
             <NavigationMenuItem>
               <Link href="/" legacyBehavior passHref>
-                <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
+                <NavigationMenuLink className="text-md text-gray-700 no-underline group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
                   Nosso Projetos
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
             <NavigationMenuItem>
               <Link href="/" legacyBehavior passHref>
-                <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
+                <NavigationMenuLink className="text-md text-gray-700 no-underline group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
                   Notícias
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
             <NavigationMenuItem>
               <Link href="/" legacyBehavior passHref>
-                <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
+                <NavigationMenuLink className="text-md text-gray-700 no-underline group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
                   Contato
                 </NavigationMenuLink>
               </Link>
@@ -49,54 +47,15 @@ export default function Home() {
           </NavigationMenuList>
         </NavigationMenu>
       </div>
-      <Carousel  
-        className='w-5/6 mb-12 h-1/2'
-        opts={{
-          loop: true,
-        }}
-        plugins={[
-          Autoplay({
-            delay: 10000,
-            stopOnInteraction: true,
-            stopOnMouseEnter: true,
-          }),
-        ]}
-      >
-        <CarouselContent>
-          <CarouselItem>
-            <Link href='/'>
-              <div className='relative mx-auto'>
-                <Image
-                  src={banner1}
-                  alt="Banner"
-                  height='0'
-                  width='0'
-                  sizes='100vw'
-                  className='w-full'
-                />
-              </div>
-            </Link>
-          </CarouselItem>
-
-          <CarouselItem>
-            <Link href='/'>
-              <div className='relative mx-auto'>
-                <Image
-                  src={banner2}
-                  alt="Banner"
-                  height='0'
-                  width='0'
-                  sizes='100vw'
-                  className='w-full'
-                />
-              </div>
-            </Link>
-          </CarouselItem>
-          
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-    </Carousel>
+      <Carousel className='flex items-center justify-center'>
+        <Carousel.Item>
+          <Image src={banner1} alt="First slide" height={900} width={1000}/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image src={banner2} alt="First slide" />
+        </Carousel.Item>
+      </Carousel>
+      
 
       
     </>
